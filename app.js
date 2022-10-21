0e8f68e30bab34d6c2785ebe25c3c555f5cb06b3
Rodrigo <rodricelis@gmail.com>
2022-10-21 17:43:10 -0400

    New Changes

--- app.js
+++
@@ -1,7 +0,0 @@
-const grid = document.getElementById('#grid');
-const a = document.getElementById('#dropdown-menu-grid');
-const h = document.getElementById('#header');
-
-h.addEventListener('click',() => {
-  a.classList.toggle('scale');
-})
